aaa3f8fcada3e0c45449ee0578f86f71051b3ef2
Preston Johnson <prestondjohnson2006@outlook.com>
2025-11-27 00:38:11 +0000

    Finished board implementation

--- board.ts
+++ board.ts
@@ -1,6 +1,12 @@
 import { piece } from './piece.ts';
+import { movement } from './movement.ts';
 
 export class board{
     boardPieces: piece[];
+    movements: movement[];
 
+    constructor(boardPieces: piece[], movements: movement[]){
+        this.boardPieces = boardPieces;
+        this.movements = movements;
+    }
 }
